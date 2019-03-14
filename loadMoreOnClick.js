2726cefa63d87fbeb00a9940bf04bcda576c8b46
Asaduzzaman Sarker <lallsobuz@gmail.com>
2019-03-14 17:52:22 +0600

    Update loadMoreOnClick.js

--- loadMoreOnClick.js
+++ loadMoreOnClick.js
@@ -1,15 +1,15 @@
 $(function() {
-  $("div")
-    .slice(0, 4)
+  $(".box-hidden")
+    .slice(0, 6)
     .show();
   $("#loadMore").on("click", function(e) {
     e.preventDefault();
-    $("div:hidden")
-      .slice(0, 4)
+    $(".box-hidden:hidden")
+      .slice(0, 6)
       .slideDown();
 
-    if ($("div:hidden").length == 0) {
-      $("#load").fadeOut("slow");
+    if ($(".box-hidden:hidden").length == 0) {
+      $("#loadMore").fadeOut("slow");
     }
 
     $("html,body").animate(
